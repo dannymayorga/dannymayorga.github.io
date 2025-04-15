79b8b7b4687ff0e176488fac0e7dd927e7e63ad3
Danny Mayorga <dannymayorga81@gmail.com>
2025-04-15 01:17:20 -0400

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Github Pages dannymayorga.com/
-export default defineConfig({
-  base: '/',
-  plugins: [react()],
-})
